1c2f9b711029e4462ccd315387e596467d2ea6c3
Amr Aboelela <amraboelela@gmail.com>
2021-06-17 11:51:36 -0700

    Added SearchBar

---
+++ Sources/AmrSwiftUI/SearchBar.swift
@@ -0,0 +1,78 @@
+//
+//  SearchBar.swift
+//  AmrSwiftUI
+//
+//  Created by Amr Aboelela on 6/17/21.
+//  Copyright © 2021 Amr Aboelela. All rights reserved.
+//
+
+import SwiftUI
+
+protocol SearchBarDelegate {
+    var searchText: String {get set}
+    func startSearch()
+    func updateData()
+}
+
+@available(iOS 13.0, *)
+@available(macOS 10.15, *)
+struct SearchBar: View {
+    @Binding var searchBarDelegate: SearchBarDelegate
+    
+    var body: some View {
+        HStack {
+            #if os(iOS)
+            HStack {
+                Image(systemName: "magnifyingglass")
+                
+                TextField("search", text: $searchBarDelegate.searchText, onEditingChanged: { isEditing in
+                    searchBarDelegate.startSearch()
+                }, onCommit: {
+                    print("onCommit")
+                })
+                .foregroundColor(.primary)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+                
+                Button(action: {
+                    searchBarDelegate.searchText = ""
+                    searchBarDelegate.updateData()
+                }) {
+                    Image(systemName: "xmark.circle.fill").opacity(searchBarDelegate.searchText == "" ? 0 : 1)
+                }
+                
+            }
+            .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+            .foregroundColor(.secondary)
+            .background(Color(.secondarySystemBackground))
+            .cornerRadius(10.0)
+            #endif
+        }
+    }
+}
+
+#if DEBUG
+class PostsViewModelTest: SearchBarDelegate {
+    var searchText = ""
+    
+    func startSearch() {
+        print("startSearch")
+    }
+    
+    func updateData() {
+        print("updateData")
+    }
+}
+
+@available(iOS 13.0, *)
+@available(macOS 10.15, *)
+struct SearchBar_Previews: PreviewProvider {
+    @State static var postsViewModelTest: SearchBarDelegate = PostsViewModelTest()
+    
+    static var previews: some View {
+        SearchBar(searchBarDelegate: $postsViewModelTest)
+    }
+}
+
+#endif
+
